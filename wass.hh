2ec81135ae14ad087e55b41d561e515d242f6e82
Tyler Masthay <tyler@lambert.oden.utexas.edu>
2022-04-06 00:34:13 -0500

    Add nonabstract impl. Code compiles without instantiation

--- wass.hh
+++ wass.hh
@@ -4,6 +4,7 @@
 #include "misfit.hh"
 #include <iostream>
 #include <cassert>
+#include <vector>
 
 using namespace std;
 template<class T> using Ctn=vector<T>;
@@ -11,59 +12,95 @@ template<class T> using Ctn=vector<T>;
 
 template< class T >
 class Wass : public Misfit<T>{
+private:
+    Ctn<T> data;
+    Ctn<Ctn<T>> data_ren;
+    Ctn<T> t;
+    Ctn<T> p;
+    int num_rec;
+    int nt;
+    int np;
+    T eps=1e-10;
 public:
-    Ctn<T> cdf(Ctn<T> f, Ctn<T> x){
+    Wass(Ctn<T> data, Ctn<T> t, Ctn<T> p, int num_rec, int nt){
+        this->data = data;
+        assert(data.size() == num_rec * nt);
+        this->t.assign(t.begin(), t.end());
+        this->p.assign(p.begin(), p.end());
+        this->num_rec = num_rec;
+        this->nt = nt;
+        this->data_ren.swap(this->renormalize(this->data));
+    }
+    Ctn<T> cdf(Ctn<T> f){
         //precondition error checking
-        assert( f.size() == x.size() );
-    
+        assert( f.size() == t.size() );
+
         int N = f.size();
-        Ctn<T> F(0,N);
+        Ctn<T> F(N,0);
         for(int i = 0; i < N - 1; i++){
-            std::cout << i << " ";
-            T df = f[i+1] - f[i];
-            T dx = x[i+1] - x[i];
-            assert( dx > 0 );
-            F[i+1] = F[i] + df/dx;
-            std::cout << i+1 << endl;
+            T avgf = 0.5 * (f[i+1] + f[i]);
+            T dt = t[i+1] - t[i];
+            assert( dt > 0 );
+            F[i+1] = F[i] + avgf * dt;
+        }
+        for(int i = 0; i < N; i++){
+            F[i] /= F[N-1];
         }
         return F;
     }
     
-    Ctn<T> quantile(Ctn<T> F, 
-        Ctn<T> x, 
-        Ctn<T> p, 
-        T eps){
+    Ctn<T> quantile(Ctn<T> F){
          //precondition error checking
         // assert( F.size() == x.size() )
          //looping vars
-         int P = p.size(); 
-         int N = x.size();
-         int i_x = 0;
+         int i_t = 0;
     
          //initialize Quantile
-         Ctn<T> Q(0, P);
+         Ctn<T> Q(np, 0);
     
-         for(int i_p = 0; i_p < P; i_p++){
-             while( i_x < N ) {
-                 if( F[i_x] <= p[i_p] and p[i_p] <= F[i_x+1] ){
-                     T df = F[i_x+1] - F[i_x];
+         for(int i_p = 0; i_p < np; i_p++){
+             for(int i_t = 0; i_t < nt - 1; i_t++){
+                 if( F.at(i_t) <= p.at(i_p) and p.at(i_p) <= F.at(i_t+1) ){
+                     T df = F[i_t+1] - F[i_t];
                      if( df < eps ){
                          //left bin -- division might cause problems
-                         Q[i_p] = F[i_x];
+                         Q.at(i_p) = F.at(i_t);
                      }
                      else{ //else interpolate between left and right
-                         Q[i_p] = (p - F[i_x]) / df;
+                         T alpha = (p.at(i_p) - F.at(i_t)) / df;
+                         Q.at(i_p) = (1-alpha) * F.at(i_t) + alpha * F.at(i_t+1);
                      }
-                     if( i_x != N-1 ) i_x++;
                  }
              }
          }
          return Q;
     }
+   
+    
+    virtual Ctn<Ctn<T>> renormalize(Ctn<T> f) = 0; // pure virtual
     
     T eval(Ctn<T> m){
-        std::cout << "Wasserstein dummy eval\n";
-        return 0.0;
+       T eps_for_now = 1e-10;
+       Ctn<Ctn<T>> m_ren(renormalize(m));
+       assert(m_ren.size() == data_ren.size());
+       assert(m_ren.at(0).size() == num_rec * nt);
+       int num_splits = m_ren.size();
+       T total_sum = 0.0;
+       for(int i_s = 0; i_s < num_splits; i_s++){
+           for(int i_r = 0; i_r < num_rec; i_r++){
+               typename Ctn<T>::iterator start = m_ren.at(i_s).begin() + i_r * nt;
+               typename Ctn<T>::iterator end = start + nt;
+               Ctn<T> f_q(quantile(cdf(Ctn<T>(start,end))));
+               Ctn<T> d_q(quantile(cdf(Ctn<T>(start,end))));
+               for(int i_t = 0; i_t < nt - 1; i_t++){
+                   T dt = t.at(i_t+1) - t.at(i_t);
+                   T dqr = f_q.at(i_t+1) - d_q.at(i_t+1);
+                   T dql = f_q.at(i_t) - d_q.at(i_t);
+                   total_sum += 0.5 * dt * (dqr*dqr + dql*dql);
+               }
+           }
+       }
+       return total_sum;
     }
 };
 
---
+++ wass_split.hh
@@ -0,0 +1,25 @@
+#ifndef WASS_SPLIT_H
+#define WASS_SPLIT_H
+
+#include "wass.hh"
+#include <vector>
+#include <cassert>
+
+using namespace std;
+template<class T> using Ctn=vector<T>;
+
+template<class T>
+class WassSplit : public Wass<T>{
+public:
+    Ctn<Ctn<T>> renormalize(Ctn<T> f){
+        Ctn<Ctn<T>> g(2,f);
+        int N = f.size();
+        for(int i = 0; i < N; i++){
+            if( g.at(0).at(i) < 0 ) g.at(0).at(i) = 0.0;
+            if( g.at(1).at(i) > 0 ) g.at(0).at(i) = 0.0;
+        }
+        return g;
+    }
+};
+
+#endif
