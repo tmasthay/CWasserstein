d39e9fd32a55326695d22cc6a9fe12d2c0a4fdb9
Tyler Masthay <tyler@lambert.oden.utexas.edu>
2022-04-17 15:23:41 -0500

    Add renormalization step

--- include/wass_split.hh
+++ include/wass_split.hh
@@ -21,11 +21,37 @@ public:
 
         //tmp var for efficiency
         int N = f.size();
-        
+ 
+        assert( N == this->nt );
+       
+        T pos_int = 0.0;
+        T neg_int = 0.0; 
         //split between positive and negative parts
         for(int i = 0; i < N; i++){
             if( g[0][i] < 0 ) g[0][i] = 0.0;
             if( g[1][i] > 0 ) g[1][i] = 0.0;
+            if( i >= 1 ){
+                T dt = t[i] - t[i-1];
+                pos_int += 0.5 * dt * (g[0][i] + g[0][i-1]);
+                neg_int += 0.5 * dt * (g[1][i] + g[1][i-1]);
+            }
+        }
+  
+        T tol=1e-20;
+        if( pos_int >= tol ){
+            T pos_int_inv = 1.0 / pos_int;
+            for(int i = 0; i < N; i++) g[0][i] = pos_int_inv * g[0][i];
+        }
+        else{
+            for(int i = 0; i < N; i++) g[0][i] = 0.0;
+        }
+
+        if( neg_int >= tol ){
+            T neg_int_inv = 1.0 / neg_int;
+            for(int i = 0; i < N; i++) g[1][i] = neg_int_int * g[1][i];
+        }
+        else{
+            for(int i = 0; i < N; i++) g[1][i] = 0.0;
         }
         return g;
     }
